fe12f78be97de75851b0195be598f757ea03ba04
Zel-Admin <axelly433@gmail.com>
2026-03-06 11:36:00 +0700

    Fix build command

---
+++ app/src/main/java/com/codedroidx/editor/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codedroidx.editor;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("CodeDroidX running");
+        setContentView(tv);
+    }
+}
